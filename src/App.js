cfdb1453905c828e34bb883620afcdbd3f685631
weestoater <ian@weestoater.com>
2020-04-13 00:42:41 +0100

    Adding some content and a footer for now

--- src/App.js
+++ src/App.js
@@ -2,12 +2,25 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
-    <Header />
-      <img src={logo} className="logo" alt="logo" />
+      <Header />
+      <div className="container main">
+        <div className="row">
+          <div className="col-sm-12">
+            <div className="jumbotron">
+            <img src={logo} className="logo" alt="logo" />
+            <h1>ianburrett ~ strava app</h1>
+            <p>My aim on this page is to provide a dynamic page which displays my recent strava activities and utilises the Strava API.</p>
+            <p>I'm intending to build this 'app' in reactjs &amp; bootstrap / font-awesome.</p>
+            </div>
+          </div>
+        </div>
+      </div>      
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Footer() {
+    return (
+        <>
+            <footer className="footer mt-auto py-3">
+                <div className="container">
+                    <span>Place sticky footer content here.</span>
+                </div>
+            </footer>
+        </>
+    );    
+}
